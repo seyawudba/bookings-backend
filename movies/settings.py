3626a3eb70a39efcc5546d2c8bff51e38129e439
Seyram Awudi <seyawudb@gmail.com>
2024-08-18 17:28:40 +0000

    Add formatting.

--- movies/settings.py
+++ movies/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from dotenv import dotenv_values
@@ -17,10 +18,11 @@ from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-config = {
-    **dotenv_values(".env/.local/postgres"),
-    **dotenv_values(".env/.local/django"),
-}
+if os.path.exists(".env/.local/postgres") and os.path.exists(".env/.local/django"):
+    config = {
+        **dotenv_values(".env/.local/postgres"),
+        **dotenv_values(".env/.local/django"),
+    }
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
