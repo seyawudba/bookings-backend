9706b50a6f130d68eb8b2077a06e046841e8ac86
Seyram Awudi <seyawudb@gmail.com>
2024-08-20 14:02:18 +0000

    Add database migrations.

--- core/models.py
+++ core/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
     email = models.EmailField(max_length=55)
-    location = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.username
