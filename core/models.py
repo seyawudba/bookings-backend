28491063dd799e6fc6f71c37a4fb3c1be9989910
Seyram Awudi <seyawudb@gmail.com>
2024-08-20 13:55:59 +0000

    Add system data models.

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-
-# from django.db import models
+from django.db import models
 
 # Create your models here.
 
 
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=55)
+    location = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.username
